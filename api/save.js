d99fd93c76c5935d13ae200cd9178a0f9e847462
andyb1984 <andrewbotsford45@hotmail.com>
2026-04-25 11:05:26 +0100

    Change GitHub token in save.js
    Updated GitHub token for API authentication.

--- api/save.js
+++ api/save.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
     return;
   }
 
-  const token = "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";
+  const token = "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";
   if (!token) return res.status(500).send("GitHub token not set");
 
   const { content } = req.body;
